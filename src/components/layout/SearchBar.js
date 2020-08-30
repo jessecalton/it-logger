42b9bb497a37401495bcfa6fbb9f14a73f30a6a9
Jesse Calton <jessecalton@gmail.com>
2020-08-29 21:10:34 -0700

    deleted old commits

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    // className 'blue' is a built-in Materialize class
+    <nav style={{ marginBottom: '30px' }} className='blue'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input id='search' type='search' />
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons'>close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/logs/EditLogModal.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const EditLogModal = () => {
+  const [message, setMessage] = useState('');
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState('');
+
+  const onSubmit = () => {
+    if (message === '' || tech === '') {
+      M.toast({ html: 'Please enter a message and tech' });
+    } else {
+      console.log(message, tech, attention);
+
+      // Clear fields
+      setMessage('');
+      setTech('');
+      setAttention(false);
+    }
+  };
+
+  return (
+    //   make sure the id matches the href prop for the corresponding <a> tag
+    <div id='edit-log-modal' className='modal' style={modalStyle}>
+      <div className='modal-content'>
+        <h4>Enter System Log</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='message'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            {/* Need label for materialize input fields */}
+            <label htmlFor='message' className='active'>
+              Log Message
+            </label>
+          </div>
+        </div>
+        {/* dropdown menu for selecting technician */}
+        <div className='row'>
+          <div className='input-field'>
+            <select
+              name='tech'
+              value={tech}
+              className='browser-default'
+              onChange={(e) => setTech(e.target.value)}
+            >
+              <option value='' disabled>
+                Select Technician
+              </option>
+              <option value='Jim Bob Jenkins'>Jim Bob Jenkins</option>
+              <option value='Q-bert Moneypenny'>Q-bert Moneypenny</option>
+              <option value='T-Bone Jones'>T-Bone Jones</option>
+            </select>
+          </div>
+        </div>
+        {/* creating a checkbox */}
+        <div className='row'>
+          <div className='input-field'>
+            <p>
+              <label>
+                <input
+                  type='checkbox'
+                  className='filled-in'
+                  checked={attention}
+                  value={attention}
+                  onChange={(e) => setAttention(!attention)}
+                />
+                <span>Needs Attention</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='modal-footer'>
+        <a
+          href='#!'
+          onClick={onSubmit}
+          className='modal-close waves-effect blue btn'
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: '75%',
+  height: '75%',
+};
+
+export default EditLogModal;
